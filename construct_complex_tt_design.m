21d6225d6b92a7b7317848df4d3d76ede5a1ae33
Ken Rayner <kenrayner1@gmail.com>
2024-01-02 13:17:32 +1300

    Created and uploaded scripts to construct (t-t) designs in real and complex space

---
+++ construct_complex_tt_design.m
@@ -0,0 +1,20 @@
+function construct_complex_tt_design(d,m,n)
+%d = dimension of vector space
+%m = order of the design
+%n = number of vectors in design
+
+%define manifold for optimisation problem
+manifold = spherecomplexfactory(d,n);
+
+%set up optimisation problem
+design.M = manifold;
+design.cost = @(X) (1/(n^2))*(sum(sum((abs(X)^(2*t)))));
+design = manoptAD(design);
+
+%sense check gradient
+checkgradient(design);
+
+%run optimisation
+[x,xcost] = trustregions(design)
+
+%% want to normalise and then evaluate cost function
