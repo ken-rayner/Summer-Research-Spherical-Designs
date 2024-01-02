cb39850059ce921f28a8ab468e279e46ce7c574c
Ken Rayner <kenrayner1@gmail.com>
2024-01-02 19:13:55 +1300

    corrected code

--- construct_complex_tt_design.m
+++ construct_complex_tt_design.m
@@ -1,4 +1,4 @@
-function construct_complex_tt_design(d,m,n)
+function construct_complex_tt_design(d,t,n)
 %d = dimension of vector space
 %m = order of the design
 %n = number of vectors in design
@@ -8,7 +8,7 @@ manifold = spherecomplexfactory(d,n);
 
 %set up optimisation problem
 design.M = manifold;
-design.cost = @(X) (1/(n^2))*(sum(sum((abs(X)^(2*t)))));
+design.cost = @(X) (1/(n^2))*(sum(sum((abs(X).^(2*t)))));
 design = manoptAD(design);
 
 %sense check gradient
@@ -16,7 +16,3 @@ checkgradient(design);
 
 %run optimisation
 [x,xcost] = trustregions(design)
-
-%verify that this is a design
-A = normc(x);
-check_complex_tt_design(A,m)
