c94f13aac6d178482ec48ce37df82132e8802874
Ken Rayner <kenrayner1@gmail.com>
2024-01-02 21:36:41 +1300

    Edited to set output of function

--- complex_normalise.m
+++ complex_normalise.m
@@ -1,8 +1,10 @@
-function complex_normalise(A)
+function B = complex_normalise(A)
 
-[r c] = size(A)
+[r c] = size(A);
+
+B = zeros(r,c);
 
 for i = 1:c
-    div(i) = sqrt(A(:,i)' * A(:,i))
-    A(:,i) = A(:,i)/div(i)
+    div(i) = sqrt(A(:,i)' * A(:,i));
+    B(:,i) = A(:,i)/div(i);
 end
