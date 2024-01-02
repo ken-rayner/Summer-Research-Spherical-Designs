22ba3b52130a0f0598f4d2694df207e5c55f9689
Ken Rayner <kenrayner1@gmail.com>
2024-01-02 22:02:24 +1300

    Corrected typo

--- construct_real_tt_design.m
+++ construct_real_tt_design.m
@@ -1,4 +1,4 @@
-function construct_real_tt_design(d,m,n)
+function construct_real_tt_design(d,t,n)
 %d = dimension of vector space
 %m = order of the design
 %n = number of vectors in design
@@ -17,5 +17,16 @@ checkgradient(design);
 %run optimisation
 [x,xcost] = trustregions(design)
 
-%% want to normalise and then evaluate cost function
+% want to normalise and then evaluate cost function
 A = normc(x)
+design.cost(A)
+
+num = 1;
+for i = 1:2:(2*t-1)
+    num = num*i;
+end
+den = 1;
+for j = d:2:(d+2*t -2)
+    den = den*j;
+end
+c_t = num/den
