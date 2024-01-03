9d5c1f508d96d97b8a800cd914a7cf9be6d8c073
Ken Rayner <kenrayner1@gmail.com>
2024-01-03 23:25:43 +1300

    Corrected typo in script

--- construct_complex_tt_design.m
+++ construct_complex_tt_design.m
@@ -8,16 +8,20 @@ manifold = spherecomplexfactory(d,n);
 
 %set up optimisation problem
 design.M = manifold;
-design.cost = @(X) (1/(n^2))*(sum(sum((abs(X'*X).^(2*t)))));
+design.cost = @(X) (1/(n^2))*(sum(sum(abs(X'*X).^(2*t))));
 design = manoptAD(design);
 
 %sense check gradient
 checkgradient(design);
 
+%GPT generated, sets minimum number of iterations
+options = struct();
+options.miniter = 10000;
+
 %run optimisation
-[x,xcost] = trustregions(design)
+[x,xcost] = trustregions(design,[],options)
 
 %normalise and then evaluate cost function
 A = complex_normalise(x)
-[design.cost(A), 1/(nchoosek((d+t-1),1))]
+[design.cost(A), 1/(nchoosek((d+t-1),t))]
 %if these two values are equal then this is indeed a spherical design
