fbbe60fd2122bfdb71fc6966cc1a966be666fa4d
Ken Rayner <kenrayner1@gmail.com>
2024-01-04 22:03:24 +1300

    Edited to allow number of iterations to be specified

--- construct_real_half_design.m
+++ construct_real_half_design.m
@@ -1,4 +1,4 @@
-function construct_real_half_design(d,m,n)
+function construct_real_half_design(d,m,n,iterations)
 %d = dimension of vector space
 %m = order of the design
 %n = number of vectors in design
@@ -14,8 +14,12 @@ design = manoptAD(design);
 %sense check gradient
 checkgradient(design);
 
+%GPT generated, sets minimum number of iterations
+options = struct();
+options.miniter = iterations;
+
 %run optimisation
-[x,xcost] = trustregions(design)
+[x,xcost] = trustregions(design,[],options)
 
 %verify that this is a real design
 check_real_design(x,m)
