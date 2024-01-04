7484a85fa25e3821c13c09f23e35eea4bed1b74f
Ken Rayner <kenrayner1@gmail.com>
2024-01-04 21:16:33 +1300

    Created julia version of "construct_complex_tt_design.m"

---
+++ constructcomplexttdesign.jl
@@ -0,0 +1,26 @@
+construct(d,t,n,i)
+    #define manifold to use
+    manifold = obliquecomplexfactory(d,n)
+    
+    #define cost function to optimise
+    cost(X::AbstractMatrix,t) = sum((X' *X)^(2*t))
+
+    #use automatic differentation to define egrad
+    egrad = ForwardDiff.gradient(cost, X)
+
+    #set up problem for optimisation
+    problem = Problem(manifold, cost, egrad; miniter = i)
+
+    #run optimisation
+    result = minimize(problem, ConjugateGradient())
+
+    #output results
+    minimized_point = result.minimizer
+    minimized_value = result.minvalue
+
+end
+
+#note that a lot of the actual source is GPT generated so must be read with scrutiny
+
+
+
