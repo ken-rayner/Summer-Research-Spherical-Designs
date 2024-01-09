cd610a5bc917ffcbd52cc2e1a36f4287e39109e4
Ken Rayner <kenrayner1@gmail.com>
2024-01-09 22:19:34 +1300

    Changed so that quaternions (represented as matrices) are normalised (Frobenius) norm

--- real_to_quaternion_arrays.m
+++ real_to_quaternion_arrays.m
@@ -6,6 +6,7 @@ q_c = real_c/4;
 
 for i = 1:q_c
     quat{i} = A(:,((i-1)*4 + 1):(i*4));
+    quat{i} = quat{i} / norm(quat{i},'fro');
     %quat{i} = quaternion(quat{i});
     %q_array(:,i) = quat{i};
 end
